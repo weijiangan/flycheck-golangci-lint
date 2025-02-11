424ba1b3a13f5548c440b7a25822932ad4b51cd6
Jimmy Yuen Ho Wong <wyuenho@users.noreply.github.com>
2025-02-11 19:10:41 +0000

    Support go-ts-mode (#17)

--- flycheck-golangci-lint.el
+++ flycheck-golangci-lint.el
@@ -102,7 +102,7 @@ See URL `https://github.com/golangci/golangci-lint'."
   :error-patterns
   ((error line-start (file-name) ":" line ":" column ": " (message) line-end)
    (error line-start (file-name) ":" line ":" (message) line-end))
-  :modes go-mode)
+  :modes (go-mode go-ts-mode))
 
 ;;;###autoload
 (defun flycheck-golangci-lint-setup ()
